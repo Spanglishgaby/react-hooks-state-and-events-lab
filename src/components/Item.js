60052df98fd90538fdfd388653367655daca08ea
Yuriko <ygabriela.arnott@gmail.com>
2022-11-02 08:54:10 -0400

    commit tree e021c4c14abc358f89859bd023f1e6587b39a3bb

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart ,setInCart] =useState(false)
+  const listClass = inCart ? "in-cart" : " "
+  const buttontext =inCart ? " Remove From Cart " : "Add to Cart"
+  const buttoncolor = inCart ? "remove" : "add"
+  const toggleCart = () => {
+   //console.log('Add to the cart')
+   setInCart( inCart => !inCart)
+  }
   return (
-    <li className="">
+    <li className={listClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttoncolor} onClick ={toggleCart} >{buttontext}</button>
     </li>
   );
 }
